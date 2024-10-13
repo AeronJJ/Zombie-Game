80d969a2fdcc44640caead4a22caa4bb958dc6e9
Blastsail <aeronjarvis01@gmail.com>
2024-10-13 23:46:28 +0100

    Clean up Source file

--- src/main.cpp
+++ src/main.cpp
@@ -2,17 +2,11 @@
 #include "ZombieGameEngine.h"
 //#include "mbed_mem_trace.h"
 
-//#include "SDLInputHandling.h"
-
-//int main(int argc, char* args[]){
 int main() {
   //mbed_mem_trace_set_callback(mbed_mem_trace_default_callback);
 
   ZombieGameEngine game;
 
-  //SDLInputHandling inputs;
-  
-  //while(!inputs.checkInputs() && !inputs.checkInputs() && !inputs.checkInputs()) {
   while(true) {
     game.run();
   }
